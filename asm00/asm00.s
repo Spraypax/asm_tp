636dfccbae1c7b22b42595eb198d3bee4f2f7412
Spraypax <114649460+Spraypax@users.noreply.github.com>
2025-09-29 15:15:59 +0200

    asm00

--- asm00/asm00.s
+++ asm00/asm00.s
@@ -1,1 +1,10 @@
 ﻿;; fichier asm00.s
+
+section .text
+global _start
+
+_start:
+    ; syscall: exit(0)
+    mov rax, 60        ; numéro du syscall exit (Linux x86_64)
+    xor rdi, rdi       ; code de retour = 0 (rdi = 0)
+    syscall            ; appel système
