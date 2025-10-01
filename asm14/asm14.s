694b1ae815ee947772566ce63057aa01fa2467a1
Spraypax <114649460+Spraypax@users.noreply.github.com>
2025-10-01 14:30:14 +0200

    asm14 (WIP)

--- asm14/asm14.s
+++ asm14/asm14.s
@@ -4,60 +4,46 @@
 ;   cat output.txt  -> Hello Universe!
 
 section .data
-msg:    db "Hello Universe!", 10
-msglen: equ $ - msg
+msg     db "Hello Universe!", 0xA
+msglen  equ $ - msg
 
 section .text
 global _start
 
 _start:
-    ; argc >= 2 ?
-    mov     rax, [rsp]
+    ; vérifier argc
+    mov     rax, [rsp]         ; argc
     cmp     rax, 2
-    jl      exit1
+    jl      .no_arg            ; si < 2 → erreur
 
-    ; filename = argv[1]
-    mov     rsi, [rsp + 16]
+    ; récupérer argv[1]
+    mov     rbx, [rsp + 16]    ; nom du fichier
 
-    ; fd = openat(AT_FDCWD, filename, O_WRONLY|O_CREAT|O_TRUNC, 0644)
-    ; sys_openat = 257, AT_FDCWD = -100
-    mov     rax, 257                ; SYS_openat
-    mov     rdi, -100               ; AT_FDCWD
-    mov     rdx, 1 + 64 + 512       ; O_WRONLY | O_CREAT | O_TRUNC
-    mov     r10, 0644               ; mode
+    ; open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644)
+    mov     rax, 2
+    mov     rdi, rbx
+    mov     rsi, 577           ; O_WRONLY | O_CREAT | O_TRUNC
+    mov     rdx, 0o644
     syscall
-    ; rax = fd (>=0) or -errno (<0)
-    test    rax, rax
-    js      exit1
-    mov     r12, rax                ; sauver fd
 
-    ; write(fd, msg, msglen)
-    mov     rax, 1                  ; SYS_write
-    mov     rdi, r12
+    ; rax = fd
+    mov     rdi, rax
+    mov     rax, 1
     mov     rsi, msg
     mov     rdx, msglen
     syscall
-    test    rax, rax
-    js      close_and_exit1
 
     ; close(fd)
-    mov     rax, 3                  ; SYS_close
-    mov     rdi, r12
+    mov     rax, 3
     syscall
-    test    rax, rax
-    js      exit1
 
     ; exit(0)
     mov     rax, 60
     xor     rdi, rdi
     syscall
 
-close_and_exit1:
-    ; tenter de fermer fd avant de quitter en erreur
-    mov     rax, 3
-    mov     rdi, r12
-    syscall
-exit1:
+.no_arg:
+    ; exit(1)
     mov     rax, 60
     mov     rdi, 1
     syscall
